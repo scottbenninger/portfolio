96d98ee9c9264b046424f9a84ab99bba051b516b
scottbenninger <sbenninger@ucsd.edu>
2025-01-24 13:37:38 -0800

    lab 3

--- global.js
+++ global.js
@@ -4,3 +4,16 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
+const navLinks = $$("nav a");
+console.log(navLinks);
+
+let currentLink = navLinks.find(
+    (a) => a.host === location.host && a.pathname === location.pathname
+  );
+  console.log(currentLink); 
+
+currentLink?.classList.add("current");
+
+
+
+  
